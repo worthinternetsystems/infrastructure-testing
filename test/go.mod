124358fc0a004943b97cfd037ecfc816c2c76e13
Ethan Ely <eely@worth.systems>
2023-06-28 15:40:33 +0200

    Update Integration Test

--- test/go.mod
+++ test/go.mod
@@ -24,7 +24,7 @@ require (
 	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
 	github.com/gruntwork-io/go-commons v0.8.0 // indirect
-	github.com/gruntwork-io/terratest v0.43.0 // indirect
+	github.com/gruntwork-io/terratest v0.43.5 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-getter v1.7.1 // indirect
@@ -59,6 +59,6 @@ require (
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20221201164419-0e50fba7f41c // indirect
 	google.golang.org/grpc v1.51.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- test/terraform_integration_test.go
+++ test/terraform_integration_test.go
@@ -5,10 +5,12 @@ import (
 	"testing"
 	"log"
 
+	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
-func IntegrationTestS3bucketWithS3Object(t *testing.T) {
+func integrationTestS3bucketWithS3Object(t *testing.T) {
 	// website::tag::2:: Construct the terraform options with default retryable errors to handle the most common
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
@@ -17,7 +19,7 @@ func IntegrationTestS3bucketWithS3Object(t *testing.T) {
 	})
 
 	// Pick a random AWS region to test in. This helps ensure your code works in all regions.
-	// awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
 
 	// website::tag::5:: Clean up resources with "terraform destroy" at the end of the test.
 	defer terraform.Destroy(t, terraformOptions)
@@ -35,11 +37,11 @@ func IntegrationTestS3bucketWithS3Object(t *testing.T) {
 		log.Fatal(err)
 	} 
 
-	// // Run `terraform output` to get the value of an output variable
-	// bucketID := terraform.Output(t, terraformOptions, "bucket_id")
+	// Run `terraform output` to get the value of an output variable
+	bucketID := terraform.Output(t, terraformOptions, "bucket_id")
 
-	// // Verify that our Bucket has versioning enabled
-	// actualStatus := aws.GetS3BucketVersioning(t, awsRegion, bucketID)
-	// expectedStatus := "Enabled"
-	// assert.Equal(t, expectedStatus, actualStatus)
+	// Verify that our Bucket has versioning enabled
+	actualStatus := aws.GetS3BucketVersioning(t, awsRegion, bucketID)
+	expectedStatus := "Enabled"
+	assert.Equal(t, expectedStatus, actualStatus)
 }
