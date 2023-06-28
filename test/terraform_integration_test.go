943d0e0130028af76d7bdf99eb339604d8aa7c7f
Ethan Ely <eely@worth.systems>
2023-06-28 15:29:50 +0200

    Add first iteration of Integration testing

---
+++ test/terraform_integration_test.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"os/exec"
+	"testing"
+	"log"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func IntegrationTestS3bucketWithS3Object(t *testing.T) {
+	// website::tag::2:: Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// website::tag::1:: Set the path to the Terraform code that will be tested.
+		TerraformDir: "../examples/our_infrastructure",
+	})
+
+	// Pick a random AWS region to test in. This helps ensure your code works in all regions.
+	// awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+
+	// website::tag::5:: Clean up resources with "terraform destroy" at the end of the test.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// website::tag::3:: Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Getting list of S3 objects command:
+
+	cmd := exec.Command(`aws s3api list-objects --bucket worth-company-docs-bucket`)
+
+	err := cmd.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	} 
+
+	// // Run `terraform output` to get the value of an output variable
+	// bucketID := terraform.Output(t, terraformOptions, "bucket_id")
+
+	// // Verify that our Bucket has versioning enabled
+	// actualStatus := aws.GetS3BucketVersioning(t, awsRegion, bucketID)
+	// expectedStatus := "Enabled"
+	// assert.Equal(t, expectedStatus, actualStatus)
+}
